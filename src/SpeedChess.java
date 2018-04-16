f3acf58547faf6b8034f7d35df397b97abccc865
Matthew Pontarolo <matthewpontarolo@umail.ucsb.edu>
2018-04-15 17:31:59 -0700

    Established basic grid creation

--- src/SpeedChess.java
+++ src/SpeedChess.java
@@ -19,6 +19,9 @@ import javafx.scene.control.TextArea;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.Region;
@@ -32,6 +35,7 @@ import javafx.geometry.Insets;
 public class SpeedChess extends BorderPane {
 
 	public SpeedChess() {
+		//Setting the top as a temporary test
 		HBox northBox = new HBox(10);
 		setTop(northBox);
 		northBox.setPadding(new Insets(10, 10, 10, 10));
@@ -43,6 +47,21 @@ public class SpeedChess extends BorderPane {
 
 		northBox.getChildren().addAll(testLabel);
 
+		//Setting up the chess grid in the center
+		GridPane center = new GridPane();
+		for (int i = 0; i < 8; i++) {
+			center.getColumnConstraints().add(new ColumnConstraints(50));
+			center.getRowConstraints().add(new RowConstraints(50));
+			for (int j = 0; j < 8; j++) {
+				Button button = new Button();
+				//GridPane.setConstraints(button, i, j);
+				//center.getChildren().addAll(button);
+				center.add(button, i, j);
+			}
+		}
+		center.setAlignment(Pos.CENTER);
+		
+		setCenter(center);
 	}
 	
 }
