cb41481dab323baf2e5a4a010f18d0d16c6f4d30
Kevin Son <thecomplexgeek@169-231-81-141.wireless.ucsb.edu>
2018-04-18 18:36:12 -0700

    tried to implement board

--- src/Board.java
+++ src/Board.java
@@ -2,14 +2,24 @@ public class Board
 {
   // 8x8 matrix to store board positions
   private int[][] spots = new int[8][8];
+  private Player[] players;
+  private int currentPlayer;
+  private final char horizontal[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
+  private final int vertical[] = {1, 2, 3, 4, 5, 6, 7, 8};
 
   public Board()
   {
-
+     //uhhh...
   }
 
   public getSpot()
   {
-    
+      //return some combination of horizontal and vertical
   }
+
+  public void addPlayer(Player player, int index)
+  {
+    //idk i thought we should use this to keep track of players??
+  }
+
 }
