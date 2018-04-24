c40b3037bad3a62002a2cd7383576cdbdbe4c53f
IlMatteo <MatthewPontarolo@gmail.com>
2018-04-24 13:22:18 -0700

    Testing moving pieces

--- src/GameHost.java
+++ src/GameHost.java
@@ -1,12 +1,12 @@
 public class GameHost {
 
-  public static Player whitePlayer = new Player(true);
-  public static Player blackPlayer = new Player(false);
+	public static Board gameBoard = new Board(whitePlayer, blackPlayer);
 
-  public static Board gameBoard = new Board(whitePlayer, blackPlayer);
+	public static Player whitePlayer = new Player(true);
+	public static Player blackPlayer = new Player(false);
 
-  public static void initialize() {
-
-  }
+	public static void initialize() {
+		
+	}
 
 }
