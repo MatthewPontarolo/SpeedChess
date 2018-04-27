95ddaa98e26bf8c5fd3ac5bb8b77ff509360b901
IlMatteo <MatthewPontarolo@gmail.com>
2018-04-27 14:52:19 -0700

    Testing images

--- src/SpeedChess.java
+++ src/SpeedChess.java
@@ -110,8 +110,10 @@ public class SpeedChess extends BorderPane {
 
 	public void redrawBoard(){
 		for (Button[] bt : buttons) {
-			for (Button b : bt)
+			for (Button b : bt) {
 				b.setText("");
+				b.setGraphic(null);
+			}
 		}
 		//Place pieces
 		Player p1 = GameHost.whitePlayer;
@@ -120,10 +122,8 @@ public class SpeedChess extends BorderPane {
 			if (p.getName() == "Pawn") {
 				Image im = new Image(getClass().getResourceAsStream(p.getName() + "WhitePiece.png"));
 				b.setGraphic(new ImageView(im));
-				b.setText("");
 			} else {
 				b.setText(p.getName());
-				b.setGraphic(null);
 			}
 		}
 		Player p2 = GameHost.blackPlayer;
