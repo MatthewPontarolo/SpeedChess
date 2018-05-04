d1e94637cc1072883c6ddb9feb17faf083c4f825
IlMatteo <MatthewPontarolo@gmail.com>
2018-05-04 14:46:23 -0700

    Retesting highlights

--- src/SpeedChess.java
+++ src/SpeedChess.java
@@ -167,6 +167,7 @@ public class SpeedChess extends BorderPane {
 			ArrayList<Point> moves = move.getPossibleMoves(selectedPiece, GameHost.gameBoard, 0);
 			*/
 			ArrayList<Point> moves = PossibleMoves.getPossibleMoves(selectedPiece, GameHost.gameBoard, selectedPiece.getPlayer());
+			System.out.println("moves : " + moves);
 			for (int i = 0; i < 8; i++) {
 				for (int j = 0; j < 8; j++) {
 					if (moves.contains(new Point(i, j))) {
