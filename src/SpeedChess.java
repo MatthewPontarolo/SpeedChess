e0404782b661a1f8258016c9bfde74d051af5e26
IlMatteo <MatthewPontarolo@gmail.com>
2018-05-07 20:25:49 -0700

    Fixed error with highlighting previous space

--- src/SpeedChess.java
+++ src/SpeedChess.java
@@ -167,7 +167,6 @@ public class SpeedChess extends BorderPane {
 			ArrayList<Point> moves = move.getPossibleMoves(selectedPiece, GameHost.gameBoard, 0);
 			*/
 			ArrayList<Point> moves = selectedPiece.getValidMoves(GameHost.gameBoard, selectedPiece.getPlayer());
-			System.out.println("moves : " + moves);
 			for (int i = 0; i < 8; i++) {
 				for (int j = 0; j < 8; j++) {
 					if (moves.contains(new Point(i, j))) {
