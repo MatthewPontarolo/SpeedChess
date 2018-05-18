33c239ae81b75541d58e6861715a5cfb9d101b94
IlMatteo <MatthewPontarolo@gmail.com>
2018-05-17 17:05:24 -0700

    Cleanup of empty files

--- src/Board.java
+++ src/Board.java
@@ -3,11 +3,6 @@ import java.util.ArrayList;
 public class Board {
 	// 8x8 matrix to store board positions
 	private Piece[][] spots = new Piece[8][8];
-	private Player[] players;
-	private int currentPlayer;
-	//private final static int A = 1;
-	//private final char horizontal[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
-	//private final int vertical[] = {1, 2, 3, 4, 5, 6, 7, 8};
 
 	public Board(Player player1, Player player2)
 	{
@@ -35,14 +30,8 @@ public class Board {
 	public void movePiece(Piece p, int x, int y) {
 		spots[p.getXPosition()][p.getYPosition()] = null;
 		spots[x][y] = p;
-		// moved into Piece::move()
-		//p.setPosition(x, y);
 		p.move(x, y);
 
-	}
-
-	public void addPlayer(Player player, int index) {
-		//idk i thought we should use this to keep track of players??
 	}
 
 }
