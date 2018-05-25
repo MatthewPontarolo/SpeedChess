92ecfe8030ead28ff26fb5433b2d80f065c1f678
Glen Taggart <glen@umail.ucsb.edu>
2018-05-24 22:42:56 -0700

    ddeploy

--- src/main/java/cs48g02s18/chessServer/Main.java
+++ src/main/java/cs48g02s18/chessServer/Main.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChessServerApplication {
+public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(ChessServerApplication.class, args);
