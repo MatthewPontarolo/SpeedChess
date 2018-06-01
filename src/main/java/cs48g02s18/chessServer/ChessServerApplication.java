d5db42af7c6398eec26d123de74c75fe3e1296aa
Glen Taggart <glen@umail.ucsb.edu>
2018-05-31 17:57:43 -0700

    Deploy

--- src/main/java/cs48g02s18/chessServer/ChessServerApplication.java
+++ src/main/java/cs48g02s18/chessServer/ChessServerApplication.java
@@ -2,9 +2,16 @@ package cs48g02s18.chessServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
 
+@Controller
 @SpringBootApplication
 public class ChessServerApplication {
+    @RequestMapping("/")
+    public String home(){
+        return "index yes";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ChessServerApplication.class, args);
