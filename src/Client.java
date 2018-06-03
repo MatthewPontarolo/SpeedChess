01cdfd22cf6da3387d6a827ae2a8dc0387263f33
IlMatteo <MatthewPontarolo@gmail.com>
2018-06-03 16:59:35 -0700

    test

--- src/Client.java
+++ src/Client.java
@@ -14,10 +14,10 @@ public class Client {
 				PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
 				BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
 		) {
-			/*String inputLine;
+			String inputLine;
 			while ((inputLine = in.readLine()) != null) {
 				GameHost.processMove(inputLine);
-			}
+			}/*
 			while (moveToSend != null) {
 				//out.println("0 1 7 1 5 34525543");
 				String parsed = moveToSend.packageToString();
--- src/Server.java
+++ src/Server.java
@@ -15,10 +15,10 @@ public class Server {
 				PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
 				BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 		) {
-			/*String inputLine;
+			String inputLine;
 			while ((inputLine = in.readLine()) != null) {
 				GameHost.processMove(inputLine);
-			}
+			}/*
 			while (moveToSend != null) {
 				//out.println("0 1 7 1 5 34525543");
 				String parsed = moveToSend.packageToString();
