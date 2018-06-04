2ea6ed15f126d3cf5bd1920fe46f57c653ba6079
IlMatteo <MatthewPontarolo@gmail.com>
2018-06-03 21:03:46 -0700

    First step forward

--- src/Server.java
+++ src/Server.java
@@ -16,8 +16,8 @@ public class Server {
 				BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 		) {
 			String inputLine;
-			System.out.println("server inreadline? " + in.readLine());
 			while ((inputLine = in.readLine()) != null) {
+				System.out.println("inputLine? " + inputLine);
 				GameHost.processMove(inputLine);
 			}
 			while (moveToSend != null) {
