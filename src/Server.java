eb9dcd77d3707230368a474326212c3d5daf13ee
Annie Bai <anbaii98@gmail.com>
2018-06-04 16:05:14 -0700

    nvm

--- src/Server.java
+++ src/Server.java
@@ -19,30 +19,7 @@ public class Server {
 				PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
 				BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 		) {
-			while (!in.ready()) {
-				//TIMER COUNTDOWN
-				System.out.println("wtf kevin");
-				Timer timer = new Timer();
-				counter = 10;
-				TimerTask task = new TimerTask()
-				{
-					public void run()
-					{
-						System.out.println(Integer.toString(counter));
-						counter--;
-						if (counter == -1)
-						{
-							timer.cancel();
-						}
-						else if(isIt)
-						{
-							timer.cancel();
-							isIt = false;
-						}
-					}
-				};
-				timer.scheduleAtFixedRate(task, 1000, 1000);
-			}
+			while (!in.ready()) { }
 			String inputLine;
 			while (in.ready()) {
 				inputLine = in.readLine();
