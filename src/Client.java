d28eb240a57d66845ca4c44a2c201f0e99517ce1
IlMatteo <MatthewPontarolo@gmail.com>
2018-06-03 17:18:17 -0700

    Testing other section

--- src/Client.java
+++ src/Client.java
@@ -14,17 +14,17 @@ public class Client {
 				PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
 				BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
 		) {
-			String inputLine;
+			/*String inputLine;
 			System.out.println("client inreadline? " + in.readLine());
 			while ((inputLine = in.readLine()) != null) {
 				//GameHost.processMove(inputLine);
-			}/*
+			}*/
 			while (moveToSend != null) {
 				//out.println("0 1 7 1 5 34525543");
 				String parsed = moveToSend.packageToString();
 				out.println(parsed);
 				moveToSend = null;
-			}*/
+			}
 		} catch (UnknownHostException e) {
 			System.err.println("Don't know about host " + hostName);
 			System.exit(1);
--- src/Server.java
+++ src/Server.java
@@ -15,17 +15,17 @@ public class Server {
 				PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
 				BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 		) {
-			String inputLine;
+			/*String inputLine;
 			System.out.println("server inreadline? " + in.readLine());
 			while ((inputLine = in.readLine()) != null) {
 				//GameHost.processMove(inputLine);
-			}/*
+			}*/
 			while (moveToSend != null) {
 				//out.println("0 1 7 1 5 34525543");
 				String parsed = moveToSend.packageToString();
 				out.println(parsed);
 				moveToSend = null;
-			}*/
+			}
 		} catch (IOException e) {
 			System.out.println("Exception caught when trying to listen on port "
 					+ portNumber + " or listening for a connection");
