1ec9e69dcfced42be12d226587c240b1b7329888
Glen Taggart <glen@umail.ucsb.edu>
2018-06-06 12:13:14 -0700

    Deploy

--- src/main/java/cs48g02s18/chessserver/DataPassBoardState.java
+++ src/main/java/cs48g02s18/chessserver/DataPassBoardState.java
@@ -1,12 +1,12 @@
 package cs48g02s18.chessserver;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import cs48g02s18.chessgame.Move;
+import cs48g02s18.chessserver.MoveData;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class DataPassBoardState {
     String boardData;
-    Move opponentLastMove;
+    MoveData opponentLastMove;
 
 
     public DataPassBoardState() {
@@ -15,21 +15,23 @@ public class DataPassBoardState {
 
     public DataPassBoardState(String boardData) {
         this.boardData = boardData;
+        opponentLastMove = null;
     }
 
     public String getBoardData() {
         return boardData;
     }
 
-    public Move getOpponentLastMove() {
+    public MoveData getOpponentLastMove() {
         return opponentLastMove;
     }
+
 
     public void setBoardData(String boardData) {
         this.boardData = boardData;
     }
 
-    public void setOpponentLastMove(Move opponentLastMove) {
+    public void setOpponentLastMove(MoveData opponentLastMove) {
         this.opponentLastMove = opponentLastMove;
     }
 
