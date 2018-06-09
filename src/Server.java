eb97f20d84fabb412f364d961e3d16982b9d6995
Kevin Son <kevinson@umail.ucsb.edu>
2018-06-08 17:47:29 -0700

    get IP set IP

--- src/Server.java
+++ src/Server.java
@@ -8,7 +8,7 @@ public class Server {
 	private static Move moveToSend = null;
 	private static boolean isIt = false;
 	private static int counter = 10;
-	private static String IP ="";
+	//private static String IP ="";
 
 	public static void setup() throws IOException {
 
@@ -43,7 +43,7 @@ public class Server {
 		{
 			String host = InetAddress.getLocalHost().toString();
 			String[] parse = host.split("/");
-			IP = parse[1];
+			String IP = parse[1];
 			System.out.println("IP: " + IP);
 		}
 		catch (Exception e)
